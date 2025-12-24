1551d72703eb95429dc661043267adacfe520483
Tal-Even-Zahav <talevenzahav@mail.tau.ac.il>
2025-12-24 14:25:40 +0200

    implementation of ex2

--- ex2.c
+++ ex2.c
@@ -1,8 +1,39 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool is_binary_palindrome(unsigned int n) {
-  // TODO
+bool is_binary_palindrome(unsigned int n) 
+{
+  int msb_idx = -1;
+  unsigned int temp = n;
+  unsigned int mask = 1;
+  unsigned int extracted_bit = 0;
+  unsigned int reversed = 0;
+  while(temp != 0)
+  {
+    temp >>= 1;
+    msb_idx += 1;
+  }
+  if (msb_idx == -1)
+    msb_idx = 0;
+  for(int j = 0; j <= msb_idx; j++)
+  {
+    if (j < msb_idx/2)
+    {
+      extracted_bit = (n & mask)<<(msb_idx-j);
+      reversed |= extracted_bit;
+    }
+    if (j >= msb_idx/2)
+    {
+      extracted_bit = (n & mask)>>(j);
+      reversed |= extracted_bit;
+    }
+    mask <<=1;
+  }
+if (n == reversed)
+  return true;
+else 
+ return false;
+
 }
 
 int main() {
