28f87e9f949f6a7c1234526663154a9302f7357b
ofek-atias <ofeqi123@gmail.com>
2025-12-31 15:58:21 +0200

    Exercise 4 almost completed. Needed to add the function calls.

--- ex4.c
+++ ex4.c
@@ -1,9 +1,36 @@
 // TODO: add includes here
 #include "parser.h"
 
+#define SHIFT_V 29
+#define SHIFT_L 25
+#define SHIFT_T 20
+#define SHIFT_SN 13
+#define SHIFT_DN 6
+#define SHIFT_P 4
+#define SHIFT_E 3
+
 // encoding the packet fields according to the layout
 static unsigned int encode_packet_header(header_t hdr) {
-  // TODO
+  unsigned int packet = 0;
+  packet |= hdr.version << SHIFT_V;
+  packet |= hdr.length << SHIFT_L;
+  packet |= hdr.type << SHIFT_T;
+  packet |= hdr.src_node << SHIFT_SN;
+  packet |= hdr.dst_node << SHIFT_DN;
+  packet |= hdr.priority << SHIFT_P;
+  packet |= hdr.encrypted << SHIFT_E;
+  
+  unsigned int count = 0;
+  unsigned int checksum = 0;
+
+  for (int i = 3; i < 32; i++)
+  {
+    if((packet >> i)&1 == 1)
+      count++;
+  }
+  checksum = count&7;
+  packet|=checksum;
+  return packet;
 }
 
 // print header
@@ -24,4 +51,6 @@ int main(int argc, const char *argv[]) {
   // TODO: encode the packet header
 
   // TODO: print the encoded header
+
+  return OK;
 }
--- parser.c
+++ parser.c
@@ -1,5 +1,7 @@
-// TODO: add your includes here
 #include "parser.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define FIELDS_LEN(flags) (sizeof(flags) / sizeof(flags_t))
 
@@ -41,17 +43,63 @@ void print_help() {
   printf("Please try again.\n");
 }
 
+static const flags_t* get_flag_info(const char* arg) {
+    for (int i = 0; i < FIELDS_LEN(flags); i++) {
+        if (strcmp(arg, flags[i].short_name) == 0) {
+            return &flags[i];
+        }
+    }
+    return NULL;
+}
+
 // parse the arguments, and fill p_hdr
 err_t parse_args(int argc, const char *argv[], header_t *p_hdr) {
 
   // TODO: check parameters
-  if(p_hdr->version < FIELDS_MIN || p_hdr->version > VERSION_MAX || p_hdr->length < FIELDS_MIN || p_hdr->length > LENGTH_MAX || p_hdr->type < FIELDS_MIN || p_hdr->type > PACKET_TYPE_MAX || p_hdr->src_node < FIELDS_MIN || p_hdr->src_node > NODE_MAX || p_hdr->dst_node < FIELDS_MIN || p_hdr->dst_node > NODE_MAX || p_hdr->priority < FIELDS_MIN || p_hdr->priority > PRIORITY_MAX || p_hdr->encrypted < FIELDS_MIN || p_hdr->encrypted > ENCRYPTED)
-    return ERR_PARAMS;
+  if (p_hdr == NULL || argv == NULL) {
+      return ERR_PARAMS;
+  }
 
   // TODO: check number of arguments
-  printf("Error: number of given arguements %d is different than expected");
+  if ((argc == 2 && strcmp(argv[1], HELP_FLAG) == 0) || (argc == 1)) {
+      print_help();
+      return OK;
+  }
 
-
+  if (argc != 15) {
+      printf("Error: number of given arguements %d is different than expected\n", argc);
+      return ERR_NUM_ARGS;
+  }
 
   // TODO: parse the arguments
+  for (int i = 1; i < argc; i += 2) {
+      const flags_t* info = get_flag_info(argv[i]);
+
+      if (info == NULL) {
+          printf("Error: Unknown flag: \"%s\"\n", argv[i]);
+          return ERR_UNKNOWN_FLAG;
+      }
+
+      if (i + 1 >= argc) { //further clarification is required
+          printf("Error: Missing argument for flag \"%s\"\n", argv[i]);
+          return ERR_MISSING_ARG;
+      }
+
+      int val = atoi(argv[i+1]);
+
+      if (val < (int)info->min || val > (int)info->max) {
+          printf("Error: value %d for flag \"%s\" (%s) is out of range [%u-%u]\n", val, info->short_name, info->long_name, info->min, info->max);
+          return ERR_INVALID_VALUE;
+      }
+
+      if (strcmp(argv[i], "-v") == 0) p_hdr->version = (unsigned char)val;
+      else if (strcmp(argv[i], "-l") == 0) p_hdr->length = (unsigned char)val;
+      else if (strcmp(argv[i], "-t") == 0) p_hdr->type = (unsigned char)val;
+      else if (strcmp(argv[i], "-s") == 0) p_hdr->src_node = (unsigned char)val;
+      else if (strcmp(argv[i], "-d") == 0) p_hdr->dst_node = (unsigned char)val;
+      else if (strcmp(argv[i], "-p") == 0) p_hdr->priority = (unsigned char)val;
+      else if (strcmp(argv[i], "-e") == 0) p_hdr->encrypted = (val != 0);
+  }
+  
+  return OK;
 }
